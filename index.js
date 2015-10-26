eeffaa66e53f258c6aedacf929f0475970bc4e3d
Christoph Wassermann <christoph.wassermann@abracon.de>
2015-10-26 18:46:19 +0100

    Added D3 zoom / pan behavior and corrected various data points

--- index.js
+++ index.js
@@ -1,73 +1,133 @@
 'use strict';
 
+var PLANET_RADIUS = 3;
+var svg = null;
+var x = null;
+var y = null;
+var zoom = null;
 var planets = null;
-var zoomLevel = 1;
-var viewBox = [-500, -500, 1000, 1000];
 
-var setViewBox = function () {
-	var size = 1000 / zoomLevel;
-	var sizeFactor = Math.sqrt(zoomLevel);
-	viewBox = [-size * 0.5, -size * 0.5, size, size];
+var setScales = function () {
+	var bbox = svg.node().getBoundingClientRect();
+	var diff = bbox.width - bbox.height;
+	var zoomTranslate = null;
+	var zoomScale = null;
+	var zoomCenter = null;
 	
-	var map = d3.select('.map').attr('viewBox', viewBox[0] + ' ' + viewBox[1] + ' ' + viewBox[2] + ' ' + viewBox[3]);
-	map.selectAll('circle').attr('r', 4 / sizeFactor);
-	map.selectAll('text')
-		.attr('style', 'font-size: ' + 100 / sizeFactor + '%;');
+	if(zoom) {
+		zoomTranslate = zoom.translate();
+		zoomCenter = zoom.center();
+		zoomScale = zoom.scale();
+		console.log(zoomTranslate[1], zoomScale);
+	}
+	
+	x = d3.scale.linear()
+		.domain([-600, 600])
+		.range([diff /2, bbox.width - diff/2]);
+	y = d3.scale.linear()
+		.domain([-600, 600])
+		.range([bbox.height, 0]);
+	
+	if(zoom) {
+		zoom.x(x).y(y).scale(zoomScale).translate(zoomTranslate);
+	}
 };
 
-var createViz = function () {
-	var map = d3.select('.map');
-	var pg = map.select('.main-ct')
-		.selectAll('circle')
+var transform = function (d, i) {
+	return 'translate('+x(d.x) + ',' + y(d.y) + ')';
+};
+
+var faction = function(d) {
+	switch(d.affiliation) {
+		case 'Lyran Commonwealth':
+			return 'blue';
+		case 'Federated Suns':
+			return 'orange';
+		case 'Draconis Combine':
+			return 'red';
+		case 'Free Worlds League':
+			return 'purple';
+		case 'Capellan Confederation':
+			return 'green';
+		case 'ComStar':
+			return 'silver';
+	}
+	if(d.affiliation.indexOf('Clan') >= 0) {
+		return 'brown';
+	}
+	if(d.affiliation !== 'No record' && d.affiliation !== '?') {
+		return 'grey';
+	}
+	return 'lightblue';
+};
+
+var setViewport = function () {
+	svg.selectAll('g')
+		.attr('transform', transform);
+};
+
+var createVisualization = function () {
+	zoom = d3.behavior.zoom()
+		.x(x)
+		.y(y)
+		.scaleExtent([0.5,8])
+		.on('zoom', setViewport)
+		.on('zoomstart', function () {
+			svg.classed('dragging', true);
+		})
+		.on('zoomend', function () {
+			svg.classed('dragging', false);
+		});
+	svg.call(zoom);
+	
+	var groups = svg.selectAll('g')
 			.data(planets)
-		.enter();
+		.enter().append('g')
+			.attr('transform', transform);
 	
-	pg.append('circle')
-			.attr('r', 5).attr('fill', 'black')
-			.attr('cx', function(d, i) { return d.x })
-			.attr('cy', function(d, i) { return -d.y })
-			.attr('fill', function(d, i) {
-				switch(d.affiliation) {
-					case 'Lyran Commonwealth':
-						return 'blue';
-					case 'Federated Suns':
-						return 'orange';
-					case 'Draconis Combine':
-						return 'red';
-					case 'Free Worlds League':
-						return 'purple';
-					case 'Capellan Confederation':
-						return 'green';
-					case 'ComStar':
-						return 'silver';
-				}
-				if(d.affiliation.indexOf('Clan') >= 0) {
-					return 'brown';
-				}
-				return 'lightblue';
-			});
-			
-	pg.append('text')
-			.attr('x', function(d) { return d.x })
-			.attr('y', function(d) { return -d.y })
-			.attr('padding', '1em 1em')
-			.text(function(d) { return d.name });
+	groups.append('circle')
+		.attr('r', PLANET_RADIUS)
+		.attr('cx', -PLANET_RADIUS*0.5)
+		.attr('cy', -PLANET_RADIUS*0.5)
+		.attr('fill', faction);
+		
+	groups.append('text')
+		.attr('x', 5)
+		.attr('y', PLANET_RADIUS*0.5)
+		.text(function (d, i) {
+			return d.name;
+		});
+	/*/var circle = b.selectAll('circle')
+			.data(planets)
+		.enter().append('circle')
+			.attr('r', 3)
+			.attr('transform', transform)
+			.attr('fill', faction);*/
 };
 
-$(document).ready(function () {
-	$.ajax('./files/planets.json').done(function (data) {
-		planets = data;
-		createViz();
-	}).fail(function (jqXHR, text, error) {
-		console.error('Could not retrieve planets.json: ', error);
-	});
-	
-	$('.map-ct').on('mousewheel', function (e) {
-		if(e.originalEvent.deltaY > 0) {
-			zoomLevel = Math.max(zoomLevel / 1.5, 0.1);
-		} else if(e.originalEvent.deltaY < 0) {
-			zoomLevel = Math.min(zoomLevel * 1.5, 20);
+var centerOnTerra = function () {
+	zoom && zoom.scale(0).translate([0,0]);
+	setViewport();
+};
+
+var main = function () {
+	svg = d3.select('svg');
+	d3.json('./files/planets.json', function (error, json) {
+		if(error) { 
+			return console.warn(error); 
 		}
-		setViewBox();
+		planets = json;
+		setScales();
+		createVisualization();
+		window.addEventListener('resize', function () {
+			setScales();
+			setViewport();
+		});
+		d3.select('body')
+			.on('keydown', function () {
+				//centerOnTerra();
+			});
 	});
-});
+};
+
+main();
