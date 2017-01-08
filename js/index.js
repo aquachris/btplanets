51b18c1169b35ff38ac834d7cc6ad71ca72e4c00
Christoph Wassermann <christoph.wassermann@googlemail.com>
2017-01-08 23:41:24 +0100

    Data fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-window.BTPLANETS_VERSION = 201610081030;
 require.config({
 	urlArgs: "v=" + window.BTPLANETS_VERSION
 });
 
-require(['js/btplanets', 'js/btplanets_keys', 'js/btplanets_routes', 'js/btplanets_ui', 'js/btplanets_userdata'], function (main, keys, routes, ui, userdata) {
+require(['js/btplanets', 'js/btplanets_keys', 'js/btplanets_routes', 'js/btplanets_ui'/* 'js/btplanets_userdata'*/], function (main, keys, routes, ui/*, userdata*/) {
 	'use strict';
 
 	var initialized = false;
