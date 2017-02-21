6bc5d392c34adb133bed2d826aaa78f35642a491
Christoph Wassermann <christoph.wassermann@googlemail.com>
2017-02-21 23:38:58 +0100

    A little more work on google sheets extraction -> map update script

---
+++ node-scripts/sheetQuery.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var google = require('googleapis');
+var sheetsConn = require('./googleSheetsConnection.js');
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+var listMajors = function(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+        auth: auth,
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var rows = response.values;
+        if (rows.length == 0) {
+            console.log('No data found.');
+        } else {
+            console.log('Name, Major:');
+            for (var i = 0; i < rows.length; i++) {
+                var row = rows[i];
+                // Print columns A and E, which correspond to indices 0 and 4.
+                console.log('%s, %s', row[0], row[4]);
+            }
+        }
+    });
+};
+
+sheetsConn.loginAndAuthorize(listMajors, {});
